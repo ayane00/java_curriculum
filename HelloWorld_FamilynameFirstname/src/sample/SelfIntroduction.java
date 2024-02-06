1861be73074f7eceed6397caf01707aa9dd49aae
ay <ay@192.168.0.4>
2024-02-06 22:46:31 +0900

    Java_1-3(コメント修正済）

--- HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -6,13 +6,16 @@ public class SelfIntroduction {
 		// TODO 自動生成されたメソッド・スタブ
 		// 氏名
 		String name= "馬場　彩寧";
-		// 趣味
+		// 年齢
 		int age = 23;
+		//趣味
+		String hobby = "韓ドラ・YouTube鑑賞";
+
 
 		/*
 		 *自己紹介を出力する処理
 		 */
-		String hobby = "韓ドラ・YouTube鑑賞";
+
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "です。");
 		System.out.println("趣味は" + hobby + "です。");
